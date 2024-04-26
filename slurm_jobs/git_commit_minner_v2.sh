85e8814ce5ebad127c4f5153ebcdccb4836db085
Andie Huang <huayo708@student.otago.ac.nz>
2024-04-26 20:02:24 +1200

    update slurm jobs

--- slurm_jobs/git_commit_minner_v2.sh
+++ slurm_jobs/git_commit_minner_v2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=git_commit_miner
-#SBATCH --output=git_commit_miner.log
-#SBATCH --error=git_commit_miner.err
-#SBATCH --nodes=10              # Request 2 nodes
+#SBATCH --job-name=git_commit_miner_v2
+#SBATCH --output=git_commit_miner_v2.log
+#SBATCH --error=git_commit_miner_v2.err
+#SBATCH --nodes=10              # Request 10 nodes
 
 python /home/huayo708/projects/pyszz_andie/commit_analyzer/git_commit_miner/git_commit_miner.py -repo_dir /home/huayo708/projects/repo -output_dir /home/huayo708/projects/data/historical_commit_data
