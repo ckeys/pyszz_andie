ebef7c3e03feea4d8cdfd593aaf73796a7739d19
Andie Huang <huayo708@student.otago.ac.nz>
2024-04-26 19:46:56 +1200

    fix slurm script

--- slurm_jobs/feature_miner_v2.sh
+++ slurm_jobs/feature_miner_v2.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=feature_miner_v2
 #SBATCH --output=feature_miner_v2.log
 #SBATCH --error=feature_miner_v2.err
-#SBATCH --ntasks-per-node=1   # Run 1 task per node
+#SBATCH --ntasks-per-node=16   # Run 1 task per node
 #SBATCH --nodes=10              # Request 2 nodes
 #SBATCH --cpus-per-task=16
 #SBATCH --time=24:00:00   # Adjust the time limit as needed
