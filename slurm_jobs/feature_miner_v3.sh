a7da2ccaf252a07bd7f605ecd2b8ebcfaabe9e8c
Andie Huang <huayo708@student.otago.ac.nz>
2024-05-01 16:22:32 +1200

    fix code

--- slurm_jobs/feature_miner_v3.sh
+++ slurm_jobs/feature_miner_v3.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=feature_miner_v3
-#SBATCH --output=feature_miner_v3.log
-#SBATCH --error=feature_miner_v3.err
-#SBATCH --nodes=10              # Request 2 nodes
-#SBATCH --time=INFINITE  # Adjust the time limit as needed
 
+# Get current timestamp
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+# Generate job name, output file, and error file based on timestamp
+job_name="feature_miner_$timestamp"
+output_file="feature_miner_$timestamp.log"
+error_file="feature_miner_$timestamp.err"
+
+#SBATCH --job-name=$job_name              # Job name
+#SBATCH --output=$output_file             # Output file for stdout
+#SBATCH --error=$error_file               # Output file for stderr
+#SBATCH --nodes=1                        # Request 10 nodes
+#SBATCH --time=INFINITE                   # Adjust the time limit as needed
 
 # Command to run
-/usr/bin/python /home/huayo708/projects/pyszz_andie/commit_analyzer/feature_miner.py -cdir /home/huayo708/projects/data/historical_commit_data -r /home/huayo708/projects/repo -o /home/huayo708/projects/data/commit_minner_data
+/usr/bin/python /home/huayo708/projects/pyszz_andie/commit_analyzer/feature_miner.py -cdir /home/huayo708/projects/data/historical_commit_data -o /home/huayo708/projects/data/commit_minner_data
