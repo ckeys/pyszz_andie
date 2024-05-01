ab87722eb24c187b111f93332a8f78379cfbefa3
Andie Huang <huayo708@student.otago.ac.nz>
2024-05-01 16:22:32 +1200

    fix code

---
+++ slurm_jobs/feature_miner_t1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=feature_miner_v3
+#SBATCH --output=feature_miner_v3.log
+#SBATCH --error=feature_miner_v3.err
+#SBATCH --nodes=10              # Request 2 nodes
+#SBATCH --time=INFINITE  # Adjust the time limit as needed
+
+
+# Command to run
+/usr/bin/python /home/huayo708/projects/pyszz_andie/commit_analyzer/feature_miner.py -cdir /home/huayo708/projects/data/historical_commit_data  -o /home/huayo708/projects/data/commit_minner_data
