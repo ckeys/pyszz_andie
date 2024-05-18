e7cfcc122a6eea2fe8a27e6a61c5edec8f3073dc
Andie Huang <huayo708@student.otago.ac.nz>
2024-05-18 20:06:00 +1200

    update slurm job script

--- slurm_jobs/mlszz_tmp.sh
+++ slurm_jobs/mlszz_tmp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=mlszz
-#SBATCH --output=mlszz.log
-#SBATCH --error=mlszz.err
+#SBATCH --job-name=mlszz_p3
+#SBATCH --output=mlszz_p3.log
+#SBATCH --error=mlszz_p3.err
 
 
 # Command to run
-/usr/bin/python /home/huayo708/projects/pyszz_andie/main.py /home/huayo708/projects/pyszz_andie/in/bugfix_commits_all.json /home/huayo708/projects/pyszz_andie/conf/mlszz.yml /home/huayo708/projects/repo 5
+/usr/bin/python /home/huayo708/projects/pyszz_andie/main.py /home/huayo708/projects/pyszz_andie/in/bugfix_commits_all.json /home/huayo708/projects/pyszz_andie/conf/mlszz.yml /home/huayo708/projects/repo 42
