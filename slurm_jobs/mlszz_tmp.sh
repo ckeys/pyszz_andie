10e07c460db1f4970e0112034da465438e7f843e
Andie Huang <huayo708@student.otago.ac.nz>
2024-05-19 18:55:04 +1200

    update code

--- slurm_jobs/mlszz_tmp.sh
+++ slurm_jobs/mlszz_tmp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=mlszz_p4
-#SBATCH --output=mlszz_p4.log
-#SBATCH --error=mlszz_p4.err
+#SBATCH --job-name=mlszz_p5
+#SBATCH --output=mlszz_p5.log
+#SBATCH --error=mlszz_p5.err
 
 
 # Command to run
-/usr/bin/python /home/huayo708/projects/pyszz_andie/main.py /home/huayo708/projects/pyszz_andie/in/bugfix_commits_all.json /home/huayo708/projects/pyszz_andie/conf/mlszz.yml /home/huayo708/projects/repo 78
+/usr/bin/python /home/huayo708/projects/pyszz_andie/main.py /home/huayo708/projects/pyszz_andie/in/bugfix_commits_all.json /home/huayo708/projects/pyszz_andie/conf/mlszz.yml /home/huayo708/projects/repo 151
