43dfa3cc9b7e9605059b4d92c721a783a7e2603b
Andie Huang <huayo708@student.otago.ac.nz>
2024-06-10 16:57:33 +1200

    update code

--- main.py
+++ main.py
@@ -179,7 +179,9 @@ def main(input_json: str, out_json: str, conf: dict(), repos_dir: str, start_ind
     with open(f"output_{szz_name}/output_write_summary.log", "w") as summary_file:
         for log_file in log_files:
             with open(f"output_{szz_name}/{log_file}", "r") as file:
-                summary_file.write(file.read() + "\n")
+                file_content = file.read()
+                if file_content.strip() != "":
+                    summary_file.write(file_content + "\n")
     # Remove individual log files
     for log_file in log_files:
         os.remove(f"output_{szz_name}/{log_file}")
