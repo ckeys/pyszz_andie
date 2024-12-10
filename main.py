d32da2c3b9bc0cefbcecfcb16729dabd8d80ee1f
Andie Huang <huayo708@student.otago.ac.nz>
2024-12-10 23:00:57 +1300

    fix code

--- main.py
+++ main.py
@@ -54,10 +54,9 @@ def main(input_json: str, out_json: str, conf: dict(), repos_dir: str, start_ind
             bug_introducing_commits = set()
             repo_name = commit['repo_name']
             # repo_url = f'https://test:test@github.com/{repo_name}.git'  # using test:test as git login to skip private repos during clone
-            username = 'ckeys'
-            st = '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'
             # repo_url = f'''https://{username}:{st}@github.com/{repo_name}.git'''
-            repo_url = f'''https://{st}@github.com/{repo_name}.git'''
+            # repo_url = f'''https://{st}@github.com/{repo_name}.git'''
+            repo_url = f'''git@github.com:{repo_name}.git'''
             fix_commit = commit['fix_commit_hash']
 
             log.info(f'[Tracking:] {i + 1} of {tot}: {repo_name} {fix_commit}')
