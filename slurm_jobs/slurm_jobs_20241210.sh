861591620379502f6527a2bac5b942720c5793ff
Andie Huang <huayo708@student.otago.ac.nz>
2024-12-10 16:13:07 +1300

    update slurm script

--- slurm_jobs/slurm_jobs_20241210.sh
+++ slurm_jobs/slurm_jobs_20241210.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=srcml_job            # Base job name
-#SBATCH --array=1-6           # Create 3 tasks
-#SBATCH --nodes=1             # Each task runs on a separate node
-#SBATCH --cpus-per-task=2               # Number of CPU cores per task
 #SBATCH --mem=8G                        # Memory allocation
-#SBATCH --output=srcml_output.%j_%A_%a.log     # Base output log file name with job ID
-#SBATCH --error=srcml_error.%j_%A_%a.log       # Base error log file name with job ID
+#SBATCH --output=srcml_output.%j.log     # Base output log file name with job ID
+#SBATCH --error=srcml_error.%j.log       # Base error log file name with job ID
 #SBATCH --partition=aoraki              # Partition name (adjust as needed)
 #SBATCH --time=03:00:00                 # Maximum time allocation
 
@@ -31,7 +28,7 @@ LAST_INDEX=$(ls $LOG_DIR/mlszz_p*.log 2>/dev/null | sed -n 's/.*mlszz_p\([0-9]\+
 
 # Initialize index if no log files are found
 if [[ -z $LAST_INDEX ]]; then
-  LAST_INDEX=2
+  LAST_INDEX=0
 fi
 
 # Set default PARAM_VALUE in case there is no previous error file
@@ -44,18 +41,18 @@ if [[ -f "$LAST_ERR_FILE" ]]; then
 fi
 
 # Set the number of jobs to submit
-START_AT=$((2 * SLURM_ARRAY_TASK_ID))
+START_AT=2
 NUM_JOBS=2  # Change this to the desired number of jobs
 BATCH_SIZE=50  # Size of each batch (i.e., 0-999 for the first job, 1000-1999 for the second job, etc.)
 
 # Loop to create and submit jobs
-for ((i=2; i<START_AT+NUM_JOBS; i++)); do
+for ((i=4; i<START_AT+NUM_JOBS; i++)); do
   # Calculate start_index and end_index for each job
   start_index=$((i * BATCH_SIZE))
   end_index=$(((i + 1) * BATCH_SIZE - 1))
 
   # Increment the index for each job
-  NEXT_INDEX=$((LAST_INDEX + i))
+  NEXT_INDEX=$((LAST_INDEX))
 
   # Set job name, log file names, and error file names with the incremented index
   JOB_NAME="mlszz_p${NEXT_INDEX}"
@@ -70,4 +67,7 @@ for ((i=2; i<START_AT+NUM_JOBS; i++)); do
 
   # Optionally, you can print information about the job that was submitted
   echo "Submitted job: $JOB_NAME with start_index=$start_index, end_index=$end_index, log: $LOG_FILE and error: $ERR_FILE"
+  # Find the highest index from existing log files
+  LAST_INDEX=$(ls $LOG_DIR/mlszz_p*.log 2>/dev/null | sed -n 's/.*mlszz_p\([0-9]\+\)\.log/\1/p' | sort -n | tail -1)
+
 done
